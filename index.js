dc81837a277916ac4430404c7b9771935c59369c
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2020-11-23 13:37:11 +0530

    Remove unnecessary comments

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ const requests = collection.item;
 
 (async function test() {
   for (const request of requests) {
-    // test request using autocannon, pass the method, body, etc. on the autocannon options
-    // you might need to manipulate your data in item so you can pass it to autocannon
     console.log(`Testing ${request.name}`);
     for (const item of request.item) {
       const result = await autocannon({
         title: item.name,
         url: item.request.url.raw,
         method: item.request.method,
-      });
+      }, console.log);
       console.log(result);
     }
   }
