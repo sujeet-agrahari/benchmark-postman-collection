d0f65084e93c2dfb1c2828acae021c1d73eb656b
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2020-11-24 10:39:06 +0530

    Add chalk and print result on each request

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
+const chalk = require('chalk');
 const autocannon = require("autocannon");
+
 const fs = require("fs");
 
-// read array of items from exported .json file from postman
 const collection = JSON.parse(fs.readFileSync("./customer-apis-v1.3.2.json", "UTF-8"));
 const requests = collection.item;
 
 (async function test() {
   for (const request of requests) {
-    console.log(`Testing ${request.name}`);
+    console.log(chalk.redBright.bold(`Testing ${request.name} - directory`));
     for (const item of request.item) {
+      console.log(chalk.blueBright(item.name));
       const result = await autocannon({
         title: item.name,
         url: item.request.url.raw,
         method: item.request.method,
-      }, console.log);
-      console.log(result);
+      });
+      console.log(autocannon.printResult(result));
     }
   }
-})().catch((error) => console.log);
+})().catch((error) => console.log(error));
